dd751452d4c4ea83b98aa3e32fb2afafc329c7f5
TJ Slade <mrtjslade@gmail.com>
2023-05-10 23:30:24 -0500

    allowed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
-    'http://127.0.0.1:5500/'
+    'http://127.0.0.1:5500/',
+    'https://material-mercury-homburg.glitch.me/',
+    'https://dazzling-snickerdoodle-777101.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
